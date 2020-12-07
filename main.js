c4c9cd9e0b0e3fe2cea098c83815f8a81781bd84
Electro-Skunk <electroskunk5@gmail.com>
2020-12-07 17:25:08 +0900

    add feature typing speed
    add feature typing speed calculate
    correct css style
    
    ! unfixed startTime issue

--- main.js
+++ main.js
@@ -1,7 +1,78 @@
 window.onload = function() {
-    let element = document.getElementById("e");
-    element.onclick = function () {
-        let win = window.open("https://electroskunk.tistory.com", "_blank");
-        win.focus();
-    };
+    let questionBox = document.getElementById("example");
+    let question = questionBox.innerHTML;
+    let answerBox = document.getElementById("answer");
+
+    // 입력 창 글자 수 제한
+    answerBox.maxlength = question.length;
+
+    // 입력 창 포커스되면 기본값 글자 지우기
+    answerBox.addEventListener("focus", function() {
+        answerBox.value = "";
+    }, false);
+
+    // 메인 이벤트
+    answerBox.addEventListener("keydown", function(event) {
+        let timer = false
+        let startTime
+        let endTime;
+        let highest;
+
+        // 타이머 시작
+        if (event.code != "Enter" && timer == false) {
+            timer = true;
+            startTime = new Date().getTime();
+            console.log("startTime =", startTime);
+
+            
+        }
+
+        // 엔터키 눌리면
+        if (event.code == "Enter") {
+            // 타이머 종료
+            endTime = new Date().getTime();
+            let ms = endTime - startTime;
+            timer = false;
+
+            console.log("startTime =", startTime);
+            console.log("endTime =", endTime);
+            console.log("ms =", ms);
+
+            // 정확도 검사
+            let answer = answerBox.value;
+            let errors = 0;
+            for (i = 0; i < question.length; i++) {
+                if (question[i] != answer[i]) {
+                    errors++;
+                }
+            }
+
+
+
+            // 결과 계산
+            let cpm = (answer.length) * (6000 / ms);
+            let grossWpm = (answer.length / 5) * (6000 / ms);
+            let netWpm = ((answer.length / 5) - errors) * (6000 / ms);
+            if (netWpm >= highest) {
+                highest = netWpm;
+            }
+            let accuracy = parseInt((1 - (errors / question.length)) * 100);
+            if (accuracy < 0)
+
+                // 결과 출력
+                document.getElementById("cpm").innerHTML = cpm;
+            document.getElementById("grossWpm").innerHTML = grossWpm;
+            document.getElementById("netWpm").innerHTML = netWpm;
+            document.getElementById("highest").innerHTML = highest;
+            document.getElementById("accuracy").innerHTML = accuracy + "%";
+        }
+    }, false);
+
+    // 항목별 설명 추가
+    let name = document.getElementsByClassName("name")
+    name[0].title = "(answer.length) * (6000 / ms)";
+    name[1].title = "(answer.length / 5) * (6000 / ms)";
+    name[2].title = "((answer.length / 5) - errors) * (6000 / ms)";
+    name[3].title = "netWpm";
+    name[4].title = "parseInt((1 - (errors / answer.length)) * 100)";
 }
